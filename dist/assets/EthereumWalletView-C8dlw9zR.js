f105b423049034e3164ea97ec25517141f84d7ba
Per-Erik Ferb <per-erik.ferb@bonniernews.se>
2024-07-28 23:15:10 +0200

    build

--- dist/assets/EthereumWalletView-C8dlw9zR.js
+++
@@ -1,1 +0,0 @@
-import{q as a,o,c as s,a as e,w as t,p as c,g as l,P as n}from"./index-BhM6tCUx.js";const r={class:"page ethereum-wallet"},u=a({__name:"EthereumWalletView",setup(_){return(i,m)=>(o(),s("div",r,[e(n,null,{icon:t(()=>[e(c)]),default:t(()=>[l(" Ethereum wallet ")]),_:1})]))}});export{u as default};
