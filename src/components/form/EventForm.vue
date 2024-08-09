82d071e9778072dda4c4da11d1e909b2e2d88497
Per-Erik Ferb <per-erik.ferb@bonniernews.se>
2024-08-09 17:39:35 +0200

    Cleanup event reactivity

--- src/components/form/EventForm.vue
+++ src/components/form/EventForm.vue
@@ -1,17 +1,17 @@
 <template>
   <Columns>
-    <TextInput id="event-summary" type="text" label="Title" v-model="model.summary" @input="handleChange" />
-    <TextInput id="event-description" type="text" label="Notes" v-model="model.description" @input="handleChange" />
-    <TextInput id="event-location" label="Location" type="text" v-model="model.location" @input="handleChange" />
-    <Input id="event-start" type="Date" label="Start" v-model="model.start" @input="handleChange" />
-    <Input id="event-end" type="Date" label="End" v-model="model.end" @input="handleChange" />
+    <TextInput id="event-summary" type="text" label="Title" v-model="model.summary" />
+    <TextInput id="event-description" type="text" label="Notes" v-model="model.description" />
+    <TextInput id="event-location" label="Location" type="text" v-model="model.location" />
+    <Input id="event-start" type="Date" label="Start" v-model="model.start" />
+    <Input id="event-end" type="Date" label="End" v-model="model.end" />
   </Columns>
 </template>
 
 <script setup>
 import TextInput from '@/components/form/Input.vue'
 import Columns from '@/components/Columns.vue'
-import { computed, defineModel, onMounted } from 'vue'
+import { computed, defineModel, onMounted, watch } from 'vue'
 import Input from '@/components/form/Input.vue'
 
 const model = defineModel({
@@ -32,19 +32,7 @@ const vEvent = computed(() => `BEGIN:VEVENT\n`
   + `DTEND:${model.value.end.replaceAll('-', '')}\n`
   + `END:VEVENT`)
 
-/*
-const vEvent = computed(() => `BEGIN:VEVENT`
-  + `UID:20020630T230445Z-3895-69-1-7@jammer`
-  + `DTSTART;VALUE=DATE:${model.value.start.replaceAll('-', '')}`
-  + `DTEND;VALUE=DATE:${model.value.end.replaceAll('-', '')}`
-  + `RRULE:FREQ=${model.value.frequency};INTERVAL=${model.value.interval};BYDAY=${model.value.byDay}`
-  + `SUMMARY:${model.value.summary}`
-  + `LOCATION:${model.value.location}`
-  + `END:VEVENT`)
-* */
-
 const emit = defineEmits(['output'])
-const handleChange = () => emit('output', vEvent.value)
-
-onMounted(handleChange)
+onMounted(() => emit('output', vEvent.value))
+watch(vEvent, val => emit('output', val))
 </script>
