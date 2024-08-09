06974f599b0d8625a4757ea3cb148b1cdd3ef3cf
Per-Erik Ferb <per-erik.ferb@bonniernews.se>
2024-08-09 17:30:40 +0200

    Repair WIFI input->output reactivity lagging
    Move emit triggering from input function to watcher

--- src/components/form/WIFIForm.vue
+++ src/components/form/WIFIForm.vue
@@ -2,14 +2,12 @@
   <Columns max-columns="two">
     <Input
       v-model="model.SSSID"
-      @input="emitOutput"
       label="SSSID"
       type="text"
       id="ssid-input" />
 
     <BaseSelector
       v-model="model.hiddenNetwork"
-      @input="emitOutput"
       :items="[{label: 'Yes', value: true}, {label: 'No', value: false}]"
       :description-extractor="item => item.label"
       :value-extractor="item => item.value"
@@ -19,13 +17,11 @@
 
     <WIFIEncryptionSelector
       v-model="model.encryptionType"
-      @input="emitOutput"
       id="wifi-encryption-selector" />
 
     <Input
       v-if="!!model.encryptionType"
       v-model="model.password"
-      @input="emitOutput"
       label="Password"
       type="text"
       id="password-input" />
@@ -33,7 +29,7 @@
 </template>
 
 <script setup>
-import { computed, defineModel, onMounted } from 'vue'
+import { computed, defineModel, onMounted, watch } from 'vue'
 import Columns from '@/components/Columns.vue'
 import WIFIEncryptionSelector from '@/components/form/WIFIEncryptionSelector.vue'
 import Input from '@/components/form/Input.vue'
@@ -54,7 +50,8 @@ const standardisedWIFIString = computed(() => `WIFI:`
   + `P:${(!!model.value.encryptionType) ? model.value.password : ''};H:${model.value.hiddenNetwork};`)
 
 const emit = defineEmits(['output'])
-const emitOutput = () => emit('output', standardisedWIFIString.value)
 
-onMounted(emitOutput)
+watch(standardisedWIFIString, val => emit('output', val))
+
+onMounted(() => emit('output', standardisedWIFIString.value))
 </script>
