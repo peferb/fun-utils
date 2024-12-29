abe1821c8b19adf13cdca44257ce3f13f82958f6
Per-Erik Ferb <per-erik.ferb@bonniernews.se>
2024-12-29 18:10:13 +0100

    Replace Vue favicon with tool icon. Fix wrong url link and made some text more clear.

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -8,10 +8,12 @@ import PageTitle from '@/components/PageTitle.vue'
     <PageTitle>About peferb/fun-utils</PageTitle>
     <p>
       This is a Vue/"Github Pages" implementation of
-      <a href="https://github.com/peferb/utils">github.com/peferb/utils</a>. If you are interested in
-      the code I would suggest taking a look at
-      <a href="https://github.com/peferb/utils">github.com/peferb/utils</a> instead, which is
-      implemented only using vanilla JS and HTML.
+      <a href="https://github.com/peferb/fun-utils">github.com/peferb/fun-utils</a>.
+    </p>
+    <p>
+      If you are only here for the code, I would suggest taking a look at
+      <a href="https://github.com/peferb/utils">github.com/peferb/utils</a> instead, which is mostly
+      the same functionality but implemented only using vanilla JS and HTML. Without the Vue clutter.
     </p>
 
     <TheTableOfContents />
